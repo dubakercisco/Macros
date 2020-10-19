9b366a89f688c819aa95af50eed415be12a46577
dubakercisco <66131773+dubakercisco@users.noreply.github.com>
2020-10-19 11:00:37 -0400

    Update ZoomPanel.js
    Adding in an Audio "ducking" feature... as well as some enhancements to timers for allowing the commands to complete before going to the next step.

--- ZoomPanel/ZoomPanel.js
+++ ZoomPanel/ZoomPanel.js
@@ -1,17 +1,39 @@
 import xapi from 'xapi';
 
+// Zoom In-Call Controls
+
 //Function to send the DTMF tones during the call.  This receives the DTMF tones from the Event Listenter below.
 
-async function sendDTMF(key, message){
- console.log(message);
- try {
-      await
-      xapi.command("Call DTMFSend", {DTMFString: key});
-     } catch (error) {
-       console.error(error);
-     }
+const sleep = (timeout) => new Promise((resolve) => {
+  setTimeout(resolve, timeout);
+});
+async function sendDTMF(code, message) {
+    console.log(message);
+
+    try {
+        const Level = await xapi.Status.Audio.Volume.get();
+        await xapi.Command.Audio.Volume.Set({
+            Level: "30",
+        });
+        console.log("Volume set to 30");
+
+        await sleep(200);
+        xapi.Command.Call.DTMFSend({
+            DTMFString: code,
+        });
+        console.log("DTMF Values Sent")
+
+        await sleep(750);
+        await xapi.Command.Audio.Volume.Set({
+            Level
+        });
+        console.log("Volume Set Back to " + Level)
+
+    } catch (error) {
+        console.error(error);
+    }
 }
- 
+
 //Event Listener - In this listener we are checking against various Widget ID's to see if they are pressed, and if they are we then send the appropriate DTMF tones to the sendDTMF function listed above.  You can edit each of these to match your WigitID's and/or add/subtract addtional case statements.
 
 xapi.Event.UserInterface.Extensions.Widget.Action.on((event) => {
